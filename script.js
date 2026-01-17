3ffdaa6396310be270fda7814f1f308a6497461d
Sh1reya1 <awasthi.shreya788@gmail.com>
2026-01-17 20:13:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const savedTasks = localStorage.getItem("tasks");
+const taskList = savedTasks ? JSON.parse(savedTasks) : [];
+
+
+// const taskList=[];  //empty arr
+const listElement=document.getElementById("taskList");
+const statusText=document.getElementById("status");
+
+//Speech Recognition
+const SpeechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition=new SpeechRecognition();
+recognition.continuous=false;
+recognition.lang='en-US';
+
+recognition.onresult=(event)=>{
+    const transcript=event.results[0][0].transcript.toLowerCase();
+    statusText.innerText=`Heard: "${transcript}"`;  
+    if(transcript.startsWith("naya task")){
+        const taskText=transcript.replace("naya task","").trim();
+        if(taskText)
+            addTask(taskText);
+    }
+    else if(transcript.startsWith("delete task")) {
+        const num=parseInt(transcript.split(" ")[2])-1;
+
+        if(!isNaN(num))
+            deleteTask(num);
+    }
+    else if(transcript.startsWith("mark task")){
+        const num=parseInt(transcript.split(" ")[2])-1;
+
+         if(!isNaN(num))
+            markTaskDone(num);
+    }
+}
+
+function addTask(task){
+    taskList.push({text:task, done:false});
+    saveTasks();
+    renderTasks();
+}
+
+function deleteTask(num){
+    if(taskList[num]){
+        taskList.splice(num,1);
+         saveTasks();
+        renderTasks();
+    }
+}
+
+function markTaskDone(num){
+    if(taskList[num]){
+        taskList[num].done=true;
+         saveTasks();
+        renderTasks();
+    }
+}
+
+function renderTasks(){
+    listElement.innerHTML="";
+    taskList.forEach((task,idx)=>{
+        const li=document.createElement("li");
+        li.innerText=`${idx+1}. ${task.text} ${task.done ?"✅":""}`
+        listElement.appendChild(li);
+    });
+}
+
+function startVoice(){
+    statusText.innerText="Listening.."
+    recognition.start();
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+}
+
+document.getElementById("startBtn").addEventListener("click",startVoice);
+renderTasks();
